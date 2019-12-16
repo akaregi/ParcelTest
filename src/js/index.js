c36e80e61b8eb880e6f6beb4cb9ea27f6acef132
akaregi <akg.tachibana@gmail.com>
2019-12-17 02:17:14 +0900

    色々変更
    Signed-off-by: akaregi <akg.tachibana@gmail.com>

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,21 @@
 import css from '../scss/style'
 import fa_js from '@fortawesome/fontawesome-free/js/all'
 import fa_css from '@fortawesome/fontawesome-free/css/all'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    if ($navbarBurgers.length <= 0) {
+        return
+    }
+
+    $navbarBurgers.forEach(el => {
+        el.addEventListener('click', () => {
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+
+            el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+        })
+    })
+})
